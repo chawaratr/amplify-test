761386871ae2026d08f13d788438a705b96adec8
chawaratr <eak.cpe18@gmail.com>
2019-11-23 11:14:41 +0700

    upd

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn AWS
+          Learn AWS By Sm@rTBoY
         </a>
       </header>
     </div>
